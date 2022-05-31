63fd8e03a37e720dba2fca1132c458ac6e2c4e01
Adam Berman <adam@returntocorp.com>
2022-05-31 09:07:35 -0700

    test pr diff

--- index.js
+++ index.js
@@ -20,3 +20,7 @@ function notExposedVuln() {
   const args = ["a", "b", "c", "d"]
   console.log(minimist(args))
 }
+
+function newlyExposedVuln() {
+  _.merge({ "obj1": "foo" }, { "obj2": "bar" })
+}
